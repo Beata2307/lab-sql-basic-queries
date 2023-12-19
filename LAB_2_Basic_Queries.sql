956ee4a53a73d5a789f62437e814a68b8898a3db
Beata2307 <beata.taudul@gmail.com>
2023-12-19 16:02:14 +0100

    Solved

---
+++ LAB_2_Basic_Queries.sql
@@ -0,0 +1,46 @@
+use sakila;
+
+-- 1. Display all available tables in the Sakila database.
+show tables;
+show full tables;
+
+-- 2. Retrieve all the data from the tables actor, film and customer.
+select * from sakila.actor;
+select * from sakila.film;
+select * from sakila.customer;
+
+-- 3. Retrieve the following columns from their respective tables:
+-- Titles of all films from the 'film' table
+select title from sakila.film;
+
+-- List of languages used in films, with the column aliased as 'language' from the 'language' table
+select * from sakila.language;
+-- List of first names of all employees from the 'staff' table
+select first_name from sakila.staff;
+
+-- 4. Retrieve unique release years.
+select distinct release_year from sakila.film;
+
+-- 5. Counting records for database insights:
+-- 5.1 Determine the number of stores that the company has.
+select count(*) from sakila.store;
+-- 5.2 Determine the number of employees that the company has.
+select count(*) from sakila.staff;
+select count(*) from sakila.actor;
+
+-- 5.3 Determine how many films are available for rent and how many have been rented.
+select count(*) from sakila.inventory;
+select count(*) from sakila.rental;
+
+-- 5.4 Determine the number of distinct last names of the actors in the database.
+select distinct last_name from sakila.actor;
+
+-- 6. Retrieve the 10 longest films.
+select distinct length from sakila.film 
+order by length desc
+limit 10;
+
+-- 7. Use filtering techniques in order to:
+--  7.1 Retrieve all actors with the first name "SCARLETT".
+select * from sakila.actor
+where first_name='SCARLETT';
